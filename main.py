4cac97ed6cabdef8aa7b7f1dc95363277e7a538d
VictorTao1998 <jit124@ucsd.edu>
2021-08-31 19:54:46 -0700

    a

--- main.py
+++ main.py
@@ -254,7 +254,7 @@ def main():
 
             if do_summary:
                 save_scalars(logger, 'train', scalar_outputs, global_step)
-                save_images(logger, 'train', image_outputs, global_step)
+                save_images(loggerawef, 'train', image_outputs, global_step)
                 #save_texts(logger, 'train', text_outputs, global_step)
             del scalar_outputs, image_outputs
 
